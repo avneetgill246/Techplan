2f04e30039bca39e2ef19a5dbe72c0548ea4b3a1
Avneet Gill <avneetsinghgill246@gmail.com>
2020-10-03 19:25:12 +0530

    quiz css 3

--- techplan/src/components/quiz/quiz.js
+++ techplan/src/components/quiz/quiz.js
@@ -1,7 +1,19 @@
 import React, { useState, useContext, useEffect, useCallback, useMemo } from 'react';
 import Button from '@material-ui/core/Button';
+import { red, purple } from '@material-ui/core/colors'
+import { createMuiTheme, ThemeProvider } from "@material-ui/core";
+
 import './quiz.css'
 
+
+const theme = createMuiTheme({
+    palette: {
+      
+       initial:{backgroundColor: '#ab47bc', color: 'white' } ,
+      select: { backgroundColor: red[500], color: 'white' },
+correct:{ backgroundColor: '#3f51b5', color: 'white' },
+    },
+  });
 export default function Quiz(params) {
 
     
@@ -15,17 +27,32 @@ export default function Quiz(params) {
     })},[])
 
     const colorchange = (ques,option)=>{
-      
-        setbuttonGraph((prev)=>{
-            prev[ques][option]=1
-            return prev
-        })
+        let ab=buttonGraph
+        let ind = ab[ques].indexOf(1)
+        if (ind==option){
+            return
+        }
+        if (ind >=0){
+            ab[ques][ind]=0
+
+        }
+            ab[ques][option]=1
+        
+        
      
+        setbuttonGraph(Array(...ab))
        
+     
+     
+    }
+    const submitQuiz = ()=>{
+
     }
    
     return(
+        <ThemeProvider theme={theme}>
         <div className="quiz">
+            
             <ol>
                 {buttonGraph.length>0?params.mainData.quiz.map((opt,index)=>(
                     <li key={index}>
@@ -36,7 +63,7 @@ export default function Quiz(params) {
                             {opt.op.map((option,index1)=>(
                                 <li key={index1}  >
                                     <Button  variant='text' className="button1" variant="contained"
-                                    color={buttonGraph[index][index1]==1?'secondary':buttonGraph[index][index1]==0?'default':'primary'}
+                                    style={buttonGraph[index][index1]==1?theme.palette.select:buttonGraph[index][index1]==0?theme.palette.initial:theme.palette.correct}
                                     onClick={()=>{colorchange(index,index1)}}
                                     
                                     >
@@ -53,10 +80,16 @@ export default function Quiz(params) {
                 )):<></>}
                
             </ol>
+           
+            <Button id='bbutton' variant="contained" style={theme.palette.correct} onClick={params.change} >
+        Submit
+      </Button>
+   
             <Button id='bbutton' variant="contained" color="primary" onClick={params.change}>
         Topics
       </Button>
         </div>
+        </ThemeProvider>
     )
 
 }
